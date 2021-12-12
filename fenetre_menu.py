6ceff9d748e48ccebb91a1abe897c497d7e0659e
pierre gaudin <pierregaudin33@hotmail.fr>
2021-12-12 16:15:38 +0100

    modification menu

--- fenetre_menu.py
+++ fenetre_menu.py
@@ -51,18 +51,65 @@ def savepart(xa,ya):
     rectangle(xa-15,ya-15,xb,yb-5,couleur="pink")
     
 
-# univ upem
+def select_type_j1():
+    while True :
+        joueur_ia1=attente_clic() #sensé etre les coordoné de la case IA du joureur choisie
+        joueur_simple1=attente_clic() #sensé etre les coordoné de la case joueur sans IA du joureur choisie
+        if joueur_ia1==joueur_ia1: 
+            return joueur_ia1
+        if joueur_simple1==joueur_simple1:
+            return joueur_simple1
+
+def select_type_j2():
+    while True :
+        joueur_ia2=attente_clic()#idem
+        joueur_simple2=attente_clic()#idem
+        if joueur_ia2==joueur_ia2:
+            return joueur_ia2
+        if joueur_simple2==joueur_simple2:
+            return joueur_simple2
+
+def select_type_j3():
+    while True :
+        joueur_ia3=attente_clic()
+        joueur_simple3=attente_clic()
+        if joueur_ia3==joueur_ia3:
+            return joueur_ia3
+        if joueur_simple3==joueur_simple3:
+            return joueur_simple3
+
+
+def select_type_j4():
+    while True :
+        joueur_ia4=attente_clic()
+        joueur_simple4=attente_clic()
+        if joueur_ia4==joueur_ia4:
+            return joueur_ia4
+        if joueur_simple4==joueur_simple4:
+            return joueur_simple4
 
 if __name__ == '__main__':
 
     cree_fenetre(1000,600)
     titrejeux()
     w=125
-    for i in range(4):
-        colonne_joueur(i,w,150,"blue")
-        w=w+200
     jouer(790,500)
     savepart(50,510)
+    for i in range(4):
+        colonne_joueur(i,w,150,"blue")
+        if i==0:
+            select_type_j1()
+            colonne_joueur(i,w,150,"green")
+        if i==1:
+            select_type_j2()
+            colonne_joueur(i,w,150,"green")
+        if i==2:
+            select_type_j3()
+            colonne_joueur(i,w,150,"green")
+        if i==3:
+            select_type_j4()
+            colonne_joueur(i,w,150,"green")
+        w=w+200
 
-    attend_clic_gauche()
+    attente_clic()
     ferme_fenetre()
